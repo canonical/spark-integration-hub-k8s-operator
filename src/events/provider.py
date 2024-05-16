f2086ed026909f5952c88ac75b424782230a0c51
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-05-16 14:43:17 +0000

    fix yaml

--- src/events/provider.py
+++ src/events/provider.py
@@ -58,28 +58,28 @@ class IntegrationHubProviderEvents(BaseEventHandler, WithLogging):
             )
 
         pod_template = f"""
-        apiVersion: kubeflow.org/v1alpha1
-        kind: PodDefault
-        metadata:
-        name: pyspark
-        namespace: {namespace}
-        spec:
-        annotations:
-            traffic.sidecar.istio.io/excludeInboundPorts: 37371,6060
-            traffic.sidecar.istio.io/excludeOutboundPorts: 37371,6060
-        args:
-        - --namespace
-        - {namespace}
-        - --username
-        - {service_account}
-        - --conf
-        - spark.driver.port=37371
-        - --conf
-        - spark.blockManager.port=6060
-        desc: Configure Canonical PySpark
-        selector:
-            matchLabels:
-            canonical-pyspark: "true"
+apiVersion: kubeflow.org/v1alpha1
+kind: PodDefault
+metadata:
+    name: pyspark
+    namespace: {namespace}
+spec:
+    annotations:
+        traffic.sidecar.istio.io/excludeInboundPorts: 37371,6060
+        traffic.sidecar.istio.io/excludeOutboundPorts: 37371,6060
+    args:
+    - --namespace
+    - {namespace}
+    - --username
+    - {service_account}
+    - --conf
+    - spark.driver.port=37371
+    - --conf
+    - spark.blockManager.port=6060
+    desc: Configure Canonical PySpark
+    selector:
+    matchLabels:
+        canonical-pyspark: "true"
         """
 
         try:
