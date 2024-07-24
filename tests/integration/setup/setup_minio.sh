274464c7f3ddee12ec9fc8d6f62e6b7d7f284bf7
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-07-24 19:01:57 +0200

    internal testing

--- tests/integration/setup/setup_minio.sh
+++ tests/integration/setup/setup_minio.sh
@@ -5,15 +5,15 @@ while [ $attempt -le 10 ]
 do
   echo "s3 params setup attempt=$attempt"
   if [ -z "$access_key" ]; then
-    access_key=$(sudo microk8s.kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_ACCESS_KEY}' | base64 -d)
+    access_key=$(microk8s.kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_ACCESS_KEY}' | base64 -d)
     echo "access_key=$access_key"
   fi
   if [ -z "$secret_key" ]; then
-    secret_key=$(sudo microk8s.kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_SECRET_KEY}' | base64 -d)
+    secret_key=$(microk8s.kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_SECRET_KEY}' | base64 -d)
     echo "secret_key=$secret_key"
   fi
   if [ -z "$endpoint_ip" ]; then
-    endpoint_ip=$(sudo microk8s.kubectl get services -n minio-operator | grep minio | awk '{ print $3 }')
+    endpoint_ip=$(microk8s.kubectl get services -n minio-operator | grep minio | awk '{ print $3 }')
     endpoint="http://$endpoint_ip:80"
     echo "endpoint=$endpoint"
   fi
