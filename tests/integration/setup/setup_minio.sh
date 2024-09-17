6a9bd28fb2ecc5223f3c2e3eb0435006a3a215c3
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-09-17 14:17:10 +0000

    Update  restart policy and fix minio credential retrivial

--- tests/integration/setup/setup_minio.sh
+++ tests/integration/setup/setup_minio.sh
@@ -5,12 +5,22 @@ while [ $attempt -le 10 ]
 do
   echo "s3 params setup attempt=$attempt"
   if [ -z "$access_key" ]; then
-    access_key=$(sudo microk8s.kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_ACCESS_KEY}' | base64 -d)
-    echo "access_key=$access_key"
+    sudo microk8s.kubectl get secret -n minio-operator microk8s-user-1
+    if [ $? -eq 0 ]; then
+      echo "Use access-key from secret"
+    else
+      echo "use default access-key"
+      access_key="minio"
+    fi
   fi
   if [ -z "$secret_key" ]; then
-    secret_key=$(sudo microk8s.kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{.data.CONSOLE_SECRET_KEY}' | base64 -d)
-    echo "secret_key=$secret_key"
+    sudo microk8s.kubectl get secret -n minio-operator microk8s-user-1
+    if [ $? -eq 0 ]; then
+      echo "secret_key=$secret_key"
+    else
+      echo "Use default secret-key"
+      secret_key="minio123"
+    fi
   fi
   if [ -z "$endpoint_ip" ]; then
     endpoint_ip=$(sudo microk8s.kubectl get services -n minio-operator | grep minio | awk '{ print $3 }')
