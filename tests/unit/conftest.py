21d9533b759e022508a4a9609d1cfdc004316e5c
Alex Batisse <alex.batisse@canonical.com>
2025-01-06 12:08:26 +0100

    tests: Update unit tests to scenario v7

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,10 +1,11 @@
 # Copyright 2024 Canonical Limited
 # See LICENSE file for licensing details.
 
+from dataclasses import replace
+
 import pytest
 from ops import pebble
-from scenario import Container, Context, Model, Mount, Relation
-from scenario.state import next_relation_id
+from ops.testing import Container, Context, Model, Mount, Relation
 
 from charm import SparkIntegrationHub
 from constants import CONTAINER, LOGGING_RELATION_NAME
@@ -53,13 +54,13 @@ def integration_hub_container(tmp_path):
         }
     )
 
-    etc = Mount("/etc/", tmp_path)
+    etc = Mount(location="/etc/", source=tmp_path)
 
     return Container(
         name=CONTAINER,
         can_connect=True,
         layers={"base": layer},
-        service_status={"integration-hub": pebble.ServiceStatus.ACTIVE},
+        service_statuses={"integration-hub": pebble.ServiceStatus.ACTIVE},
         mounts={"etc": etc},
     )
 
@@ -67,13 +68,15 @@ def integration_hub_container(tmp_path):
 @pytest.fixture
 def s3_relation():
     """Provide fixture for the S3 relation."""
-    relation_id = next_relation_id(update=True)
-
-    return Relation(
+    relation = Relation(
         endpoint=S3_RELATION_NAME,
         interface="s3",
         remote_app_name="s3-integrator",
-        relation_id=relation_id,
+    )
+    relation_id = relation.id
+
+    return replace(
+        relation,
         local_app_data={"bucket": f"relation-{relation_id}"},
         remote_app_data={
             "access-key": "access-key",
@@ -89,13 +92,15 @@ def s3_relation():
 @pytest.fixture
 def azure_storage_relation():
     """Provide fixture for the Azure storage relation."""
-    relation_id = next_relation_id(update=True)
-
-    return Relation(
+    relation = Relation(
         endpoint=AZURE_RELATION_NAME,
         interface="azure",
         remote_app_name="azure-storage-integrator",
-        relation_id=relation_id,
+    )
+    relation_id = relation.id
+
+    return replace(
+        relation,
         local_app_data={"container": f"relation-{relation_id}"},
         remote_app_data={
             "container": "my-bucket",
@@ -111,13 +116,15 @@ def azure_storage_relation():
 @pytest.fixture
 def s3_relation_tls():
     """Provide fixture for the S3 relation."""
-    relation_id = next_relation_id(update=True)
-
-    return Relation(
+    relation = Relation(
         endpoint=S3_RELATION_NAME,
         interface="s3",
         remote_app_name="s3-integrator",
-        relation_id=relation_id,
+    )
+    relation_id = relation.id
+
+    return replace(
+        relation,
         local_app_data={"bucket": f"relation-{relation_id}"},
         remote_app_data={
             "access-key": "access-key",
@@ -134,13 +141,10 @@ def s3_relation_tls():
 @pytest.fixture
 def logging_relation():
     """Provide fixture for the logging relation."""
-    relation_id = next_relation_id(update=True)
-
     return Relation(
         endpoint=LOGGING_RELATION_NAME,
         interface="logging",
         remote_app_name="grafana-agent-k8s",
-        relation_id=relation_id,
         remote_app_data={"promtail_binary_zip_url": "{}"},
         remote_units_data={
             0: {
