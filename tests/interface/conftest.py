f9bd0b43fb3d5aa2860f990da74b5fdb2c3d5a0d
Alex Batisse <alex.batisse@canonical.com>
2025-04-09 14:15:23 +0200

    tests: Add interface tester

---
+++ tests/interface/conftest.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Canonical Limited
+# See LICENSE file for licensing details.
+
+import pytest
+from interface_tester import InterfaceTester
+from scenario.state import State
+
+from charm import SparkIntegrationHub
+
+
+@pytest.fixture
+def interface_tester(interface_tester: InterfaceTester):
+    interface_tester.configure(
+        charm_type=SparkIntegrationHub,
+        state_template=State(
+            leader=True,  # we need leadership
+        ),
+    )
+
+    # this fixture needs to yield (NOT RETURN!) interface_tester again
+
+    yield interface_tester
