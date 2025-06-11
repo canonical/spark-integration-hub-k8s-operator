b166d7e374a116eba40b2f948ba1571c79d299f6
Alex Batisse <alex.batisse@canonical.com>
2025-06-11 08:37:42 +0200

    chore: Remove unused DotSerializer

--- src/common/utils.py
+++ src/common/utils.py
@@ -69,29 +69,3 @@ class WithLogging:
             return x
 
         return wrap
-
-
-class DotSerializer:
-    """A utility class that can serialize a string so as to remove the dot characters from it.
-
-    Dots are replaced with underscores and underscores are converted to double underscores.
-    """
-
-    _SPECIAL_CHAR = "§"
-
-    def __init__(self, replacement_char: str = "_"):
-        self.replacement_char = replacement_char
-
-    def serialize(self, input_string: str) -> str:
-        """Serialize the string to remove dot characters."""
-        return input_string.replace(self.replacement_char, self.replacement_char * 2).replace(
-            ".", self.replacement_char
-        )
-
-    def deserialize(self, input_string: str) -> str:
-        """Deserialize the string to original form."""
-        return (
-            input_string.replace(self.replacement_char * 2, self._SPECIAL_CHAR)
-            .replace(self.replacement_char, ".")
-            .replace(self._SPECIAL_CHAR, self.replacement_char)
-        )
