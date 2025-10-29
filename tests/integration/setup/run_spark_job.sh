6b50f1c0cfea524a951c87c617321fd526f13cbb
Alex Batisse <alex.batisse@canonical.com>
2025-10-29 11:20:32 +0100

    tests: Run spark job using 4.0.1

--- tests/integration/setup/run_spark_job.sh
+++ tests/integration/setup/run_spark_job.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-spark-client.spark-submit -v --username $1 --namespace $2 --conf spark.kubernetes.executor.request.cores=0.1 --class org.apache.spark.examples.SparkPi local:///opt/spark/examples/jars/spark-examples_2.12-3.5.5.jar 10000
+spark-client.spark-submit -v --username $1 \
+--namespace $2 \
+--conf spark.kubernetes.executor.request.cores=0.1 \
+--conf spark.kubernetes.container.image=ghcr.io/canonical/charmed-spark@sha256:5ee407585ff35d04cc6ec82a87150e43ccbaec337de06c9a2b12cd95798031ab \
+--class org.apache.spark.examples.SparkPi local:///opt/spark/examples/jars/spark-examples_2.13-4.0.1.jar 10000
